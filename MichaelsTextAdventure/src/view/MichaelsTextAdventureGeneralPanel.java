d2857ed85d71e08b6d1cb8204a45806c533ae1e3
Annie de Bry <89087287+anniedebry@users.noreply.github.com>
2023-02-16 19:53:12 -0700

    added more stuff

--- MichaelsTextAdventure/src/view/MichaelsTextAdventureGeneralPanel.java
+++ MichaelsTextAdventure/src/view/MichaelsTextAdventureGeneralPanel.java
@@ -1,15 +1,27 @@
 package view;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SpringLayout;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 public class TextAdventureGeneralPanel extends JPanel{
 	private Control controller;
 	private JPanel menuPanel; 
 	private SpringLayout layout;
+	private JScrollPane textPane;
+	private JTextField textField;
+	private JTextPane displayTextPane;
 	
 	public TextAdventureGeneralPanel(Control controller) {
 		super();
@@ -19,9 +31,13 @@ public class TextAdventureGeneralPanel extends JPanel{
 	public void setupPanel() {
 		this.setLayout(layout);
 		this.add(menuPanel);
+		this.textField = new JTextField("Type here", 50);
+		this.textPane = new JScrollPane(displayTextPane);
 		this.layout = new SpringLayout();
+		this.setLayout(layout);
 		this.menuPanel = new JPanel(new GridLayout(0, 1));
 		
+		setupTextPane();
 		updateDisplay();
 		setupListeners();
 	}
@@ -52,5 +68,21 @@ public class TextAdventureGeneralPanel extends JPanel{
 		
 //		sentences.addActionListener(click -> controller.getFrame().changeScreen("Sentences"));
 //		alphabet.addActionListener(click -> controller.getFrame().changeScreen("Alphabet"));
+	}
+	
+	public void setupTextPane() {
+		//displayTextPane
+		this.displayTextPane = new JTextPane();
+		displayTextPane.setBackground(new Color(255, 240, 194));
+		displayTextPane.setText("test");
+		SimpleAttributeSet set = new SimpleAttributeSet();
+		//setting the font
+		StyleConstants.setItalic(set, true); //change later
+		//document creation to add new strings
+		StyledDocument doc = (StyledDocument) displayTextPane.getDocument();
+		Style style = doc.addStyle("StyleName", null);
+		//adding displayTextPane to the frame
+		displayTextPane.setEditable(false); 
+		this.add(displayTextPane);
 	}
 }
