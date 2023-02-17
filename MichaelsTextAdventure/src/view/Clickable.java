d1c2500d7e7217858ab28a22abf6e376acce6393
Annie de Bry <89087287+anniedebry@users.noreply.github.com>
2023-02-16 18:52:26 -0700

    added clickable interface for buttons

---
+++ MichaelsTextAdventure/src/view/Clickable.java
@@ -0,0 +1,13 @@
+package view;
+
+/**
+ * interface used for clicking buttons
+ * @author Annie de Bry
+ * @version Febuary 2023
+ *
+ */
+
+public interface Clickable 
+{
+	public boolean consumeClick();
+}
